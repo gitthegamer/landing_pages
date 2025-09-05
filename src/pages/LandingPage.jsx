f19cd6780342b45302ea4ee55aedf863547b4737
WeiXian0524 <e005@thegamer.dev>
2025-09-05 15:05:18 +0800

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,6 +5,17 @@ import { useTranslation } from "react-i18next";
 
 const LandingPage = () => {
   const { t } = useTranslation();
+
+  const handleClick = () => {
+    if (window?.fbq) {
+      window.fbq("track", "Contact", {
+        method: "form",
+        content_name: "Contact Us Button",
+      });
+    }
+    window.open("http://t.me/TheGamer_77");
+  };
+
   return (
     <Page>
       <div className="container">
@@ -14,7 +25,7 @@ const LandingPage = () => {
               <div className="position-relative col-9 ">
                 <img className="w-100" src="/assets/image/others/btn.gif" />
                 <Button
-                  onClick={() => window.open("http://t.me/TheGamer_77")}
+                  onClick={handleClick}
                   className="position-absolute start-0 top-0 w-100 h-100 d-flex-center "
                 ></Button>
               </div>
